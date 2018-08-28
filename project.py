600106270cf799b81dff52315188e17165e1d8e5
Austin Talbot <austintalbot@gmail.com>
2018-08-27 23:11:59 -0600

    modified to use the access_token instead of credentials

--- project.py
+++ project.py
@@ -121,31 +121,31 @@ def gconnect():
 # Disconnect - revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
-    credentials = login_session.get('credentials')
-    if credentials is None:
-        response = make_response(json.dumps('Current user not connected'),401)
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print ('Access Token is None')
+        response = make_response(json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    # Execute HTTP get request to revoke current token 
-    access_token = credentials.access_token
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' %access_token
+    print ('In gdisconnect access token is %s', access_token)
+    print ('User name is: ')
+    print (login_session['username'])
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-
-    if result['status']==200:
-        # Reset the user's session
-        del login_session['credentials']
+    print ('result is ')
+    print (result)
+    if result['status'] == '200':
+        del login_session['access_token']
         del login_session['gplus_id']
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-
-        response = make_response(json.dumps('Successfully disconnected.',200))
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        # for whatever reason, the given token was invalid
-        response = make_response(json.dumps('Failed to revoke token for given user.'),400)
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
 
