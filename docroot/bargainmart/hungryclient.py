3f1f8a46e29031a769768245cf32294146e18635
Austin Talbot <austintalbot@gmail.com>
2018-09-11 23:36:49 -0600

    rate limit complete

--- docroot/bargainmart/hungryclient.py
+++ docroot/bargainmart/hungryclient.py
@@ -1,15 +1,16 @@
 from time import sleep
 import json
 import httplib2
+
 h = httplib2.Http()
-url = input(
-    "Please enter the uri you want to access, \n If left blank the connection will be set to 'http://localhost:5000/catalog':   "
-)
-if url == '':
-    url = 'http://localhost:5000/catalog'
+
+address = 'http://localhost:5000/catalog'
+
+# if url == "":
+#     url = "http://localhost:5000/catalog"
 
 req_per_minute = float(
-    input("Please specify the number of requests per minute:  "))
+    eval(input("Please specify the number of requests per minute:  ")))
 interval = (60.0 / req_per_minute)
 
 
@@ -19,17 +20,21 @@ def SendRequests(url, req_per_minute):
         result = json.loads(h.request(url, 'GET')[1])
         #result = h.request(url,'GET')[1]
         print(result)
+
         if result.get('error') is not None:
-            print("Error #%s : %s" % (result.get('error'), result.get('data')))
+            print(
+                ("Error #%s : %s" % (result.get('error'), result.get('data'))))
             print("Hit rate limit. Waiting 5 seconds and trying again...")
             sleep(5)
             SendRequests(url, req_per_minute)
+
         else:
-            print("Number of Requests: ", requests + 1)
+            print(("Number of Requests: ", requests + 1))
             print(result)
+
         requests = requests + 1
         sleep(interval)
 
 
 print("Sending Requests...")
-SendRequests(url, req_per_minute)
+SendRequests(address, req_per_minute)
